98003d2d16e5f91d95fe07f6ab183ccc000ed458
Ruizhi He <jack01best@gmail.com>
2015-07-29 12:18:47 +0100

    Insert year one course info -- Test

---
+++ test_insert_course_info.sql
@@ -0,0 +1,7 @@
+INSERT INTO timetable.course (code, name, hour, class, for_specialism, number_students, lectures)
+VALUES('co112', 'hardware', '3', '1', 'none', 'medium', 'bschulle_dfg'),
+('co120', 'programming', '5', '1', 'none', 'medium', 'ajf_tora'),
+('co140', 'logic', '3', '1', 'none', 'medium', 'imh'),
+('co145', 'mathematical_methods', '8', '1', 'none', 'medium', 'jb'),
+('co142', 'discrete_mathematics', '3', '1', 'none', 'medium', 'svb'),
+('co161', 'lab', 'lab', '1', 'none', 'medium', 'tora');
