5526f46918d162deb2acb1666a99f82523ec0c4d
Ruizhi He <jack01best@gmail.com>
2015-07-31 11:52:20 +0100

    Insert room info

---
+++ insert_rooms.sql
@@ -0,0 +1,10 @@
+INSERT INTO timetable.room (name, type)
+VALUES ('219', 'lab'),
+('202', 'lab'),
+('144', 'medium'),
+('145', 'medium'),
+('308', 'large'),
+('311', 'large'),
+('341', 'small'),
+('342', 'small'),
+('ihc', 'ihc');
