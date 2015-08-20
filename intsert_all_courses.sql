4bf60aabff83f8e9bae0b1d1ed5f920cf176264b
rh514 <ruizhi.he14@imperial.ac.uk>
2015-08-20 16:51:43 +0100

    Some changes but not finished

--- intsert_all_courses.sql
+++ intsert_all_courses.sql
@@ -1,10 +1,10 @@
-INSERT INTO timetable.course (code, name, hour, class, for_specialism, number_students, lectures)
-VALUES('co112', 'hardware', '3', '1', 'none', 'medium', 'bschulle_dfg'),
-('co120', 'programming', '5', '1', 'none', 'medium', 'ajf_tora'),
-('co140', 'logic', '3', '1', 'none', 'medium', 'imh'),
-('co145', 'mathematical_methods', '8', '1', 'none', 'medium', 'jb'),
-('co142', 'discrete_mathematics', '3', '1', 'none', 'medium', 'svb'),
-('co161', 'lab', 'lab', '1', 'none', 'medium', 'tora'),
+INSERT INTO timetable.course (code, name, type hour, class, for_specialism, number_students, lecturer1, lecturer2)
+VALUES('co112', 'hardware', 'lecture', '3', '1', 'none', 'medium', 'bschulle', 'dfg'),
+('co120', 'programming', 'lecture', '5', '1', 'none', 'medium', 'ajf', 'tora'),
+('co140', 'logic', 'lecture', '3', '1', 'none', 'medium', 'imh', 'none'),
+('co145', 'mathematical_methods', 'lecture', '8', '1', 'none', 'medium', 'jb', 'none'),
+('co142', 'discrete_mathematics', 'lecture', '3', '1', 'none', 'medium', 'svb', 'none'),
+('co161', 'lab', 'lab', '6', '1', 'none', 'medium', 'tora', 'none'),
 ('co220', 'software_engineering_design', '2', '2', 'none', 'medium', 'rbc'),
 ('co220', 'software_engineering_design_lab', 'lab', '2', 'none', 'medium', 'rbc'),
 ('co221', 'compilers', '5', '2', 'none', 'medium', 'nd_phjk'),
